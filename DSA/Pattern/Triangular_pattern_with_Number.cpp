07a6f6a124993a9485279c0b6fe1dd1f554d4a9b
Pulkit-barola <153481980+Pulkit-barola@users.noreply.github.com>
2024-10-20 21:05:21 +0530

    we can create traingular pattern with number

---
+++ DSA/Pattern/Triangular_pattern_with_Number.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A;
+    cout<<"Enter any number :- "<<endl;
+    cin>>A;
+
+   
+   for (int i = 1; i <A+1; i++)
+   {
+        for (int j = 1; j < i+1; j++)
+        {
+            
+            cout<<i;
+        }
+        cout<<endl;
+   }
+   
+}
