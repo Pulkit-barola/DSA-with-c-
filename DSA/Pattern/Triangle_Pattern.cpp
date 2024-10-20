1ae040564b4b8fd77c4ccc2b691d732444edec22
Pulkit-barola <153481980+Pulkit-barola@users.noreply.github.com>
2024-10-20 19:19:40 +0530

    making a triangular pattern

---
+++ DSA/Pattern/Triangle_Pattern.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int P;
+    cout<<"Enter any number :- ";
+    cin>>P;
+   
+    for (int i = 1; i < P+1; i++)
+    {
+        for (int j = 1; j < i+1; j++)
+        {
+            cout<<"*";
+
+        }
+        cout<<endl;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+// *
+// **
+// ***
+// ****
