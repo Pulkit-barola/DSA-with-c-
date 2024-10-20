304bf61d82c8c7ce444cfa4ef12c029d626c1fec
Pulkit-barola <153481980+Pulkit-barola@users.noreply.github.com>
2024-10-20 15:36:22 +0530

    Create Day1.cpp

---
+++ DSA/Day1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+//calculator
+int main(){
+    int A,B;
+    cout<<"Enter the first digit "<<endl;
+    cin>> A;
+    cout<<"Enter the second digit"<<endl;
+    cin>> B;
+     cout<<"The sum is :- "<<A+B<<endl;
+     cout<<"The sub is :- "<<A-B<<endl;
+    cout<<"The mult is :- "<<A*B<<endl;
+    cout<<"The div is :- "<<A/B<<endl;
+}
